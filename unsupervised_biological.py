660ffc73939e36b2056431e920e6722c71e8059d
yc-jang <jyc4261@gmail.com>
2021-04-12 08:56:29 +0000

    unsupervised_biological python file added. DimaKrotov/Biological_Learning ipynb file Ref.

--- unsupervised_biological.py
+++ unsupervised_biological.py
@@ -1,5 +1,70 @@
-
+#%%
+#[cell loads the data and normalizes it to the [0,1] range]
+import warnings
 import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+
 mat = scipy.io.loadmat('mnist_all.mat')
+
+Nc=10
+N=784
+Ns=60000
+M=np.zeros((0,N))
+for i in range(Nc):
+    M=np.concatenate((M, mat['train'+str(i)]), axis=0)
+M=M/255.0
+
+def draw_weights(fig, synapses, Kx, Ky):
+    yy=0
+    HM=np.zeros((28*Ky,28*Kx))
+    for y in range(Ky):
+        for x in range(Kx):
+            HM[y*28:(y+1)*28,x*28:(x+1)*28]=synapses[yy,:].reshape(28,28)
+            yy += 1
+    plt.clf()
+    nc = np.amax(np.absolute(HM))
+    im = plt.imshow(HM, cmap='bwr', vmin=-nc, vmax=nc)
+    plt.axis('off')
+    fig.colorbar(im, ticks=[np.amin(HM), 0, np.amax(HM)])
+    fig.canvas.draw()
+
+eps0=2e-2    # learning rate
+Kx=10
+Ky=10
+hid=Kx*Ky    # number of hidden units that are displayed in Ky by Kx array
+mu=0.0
+sigma=1.0
+Nep=200      # number of epochs
+Num=100      # size of the minibatch
+prec=1e-30
+delta=0.4    # Strength of the anti-hebbian learning
+p=2.0        # Lebesgue norm of the weights
+k=2          # ranking parameter, must be integer that is bigger or equal than 2
+
+fig = plt.figure(figsize=(12.9,10))
+
+synapses = np.random.normal(mu, sigma, (hid, N))
+for nep in range(Nep):
+    eps=eps0*(1-nep/Nep)
+    M=M[np.random.permutation(Ns),:]
+    for i in range(Ns//Num):
+        inputs=np.transpose(M[i*Num:(i+1)*Num,:])
+        sig=np.sign(synapses)
+        tot_input=np.dot(sig*np.absolute(synapses)**(p-1),inputs)
+        
+        y=np.argsort(tot_input,axis=0)
+        yl=np.zeros((hid,Num))
+        yl[y[hid-1,:],np.arange(Num)]=1.0
+        yl[y[hid-k],np.arange(Num)]=-delta
+        
+        xx=np.sum(np.multiply(yl,tot_input),1)
+        ds=np.dot(yl,np.transpose(inputs)) - np.multiply(np.tile(xx.reshape(xx.shape[0],1),(1,N)),synapses)
+        
+        nc=np.amax(np.absolute(ds))
+        if nc<prec:
+            nc=prec
+        synapses += eps*np.true_divide(ds,nc)
+        
+    draw_weights(fig, synapses, Kx, Ky)
